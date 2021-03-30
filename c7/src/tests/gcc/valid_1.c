b08b7a83098e979e3d3aa403f0e3bf3bee4a4238
Dayanne Fernandes <dayannefernandesc@gmail.com>
2021-03-29 22:32:56 -0300

    add test file to test with gcc

---
+++ c7/src/tests/gcc/valid_1.c
@@ -0,0 +1,12 @@
+int main() {
+    char test_char = 'a';
+    char test_char_4;
+    test_char_4 = 'a';
+    char test_char_2 = '\n';
+    char test_char_5;
+    test_char_5 = '\n';
+    char test_char_3 = 'string';
+    char test_char_6;
+    test_char_6 = 'stringadadsadasddsda';
+    char* test_str = "";
+}
